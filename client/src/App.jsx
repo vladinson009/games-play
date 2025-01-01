1d8521f13b0f98b9a5ed1bc2d95b67d608939a71
vladinSon <vladinson009@icloud.com>
2025-01-02 00:27:02 +0100

    created session provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import Header from "./components/header/Header";
 import HomePage from "./components/home/HomePage";
-
 import CataloguePage from './components/games/CataloguePage';
 import CreatePage from './components/games/CreatePage';
 import DetailsPage from './components/games/DetailsPage';
 import EditPage from './components/games/EditPage';
 import LoginPage from './components/user/LoginPage';
 import RegisterPage from './components/user/RegisterPage';
+import LogoutPage from './components/user/LogoutPage.jsx';
 
+import sessionCtx from './util/sessionContext.js';
 
 function App() {
+  const [session, setSession] = useState({})
+  const sessionContext = sessionCtx
+  useEffect(() => {
+    const session = JSON.parse(sessionStorage.getItem('auth'))
+    if (session) {
+      setSession(session)
+    }
+  }, [])
 
   return (
-    <>
+    <sessionContext.Provider value={{ session, setSession }}>
       <Header />
       <main id="main-content">
         <Routes>
@@ -28,11 +38,13 @@ function App() {
           <Route path="/users">
             <Route path="login" element={<LoginPage />} />
             <Route path="register" element={<RegisterPage />} />
+            <Route path="logout" element={<LogoutPage />} />
           </Route>
         </Routes>
       </main>
-    </>
+    </sessionContext.Provider >
   )
+
 }
 
 export default App
