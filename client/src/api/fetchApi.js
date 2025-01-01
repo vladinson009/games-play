2ae373de86152ae05d3cdc0a4b180db567820781
vladinSon <vladinson009@icloud.com>
2025-01-01 21:00:46 +0100

    init project

---
+++ client/src/api/fetchApi.js
@@ -0,0 +1,39 @@
+const BASE_URL = '';
+
+async function requester(url, options) {
+  const response = await fetch(BASE_URL + url, options);
+  if (!response.ok) {
+    throw new Error(`${response.status} - ${response.statusText}`);
+  }
+  if (response.status === 204) {
+    return response;
+  }
+  return response.json();
+}
+
+function createOptions(method, body) {
+  const options = {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+  return options;
+}
+
+function get(url) {
+  return requester(url, createOptions('GET'));
+}
+function post(url, body) {
+  return requester(url, createOptions('POST', body));
+}
+function put(url, body) {
+  return requester(url, createOptions('PUT', body));
+}
+function del(url) {
+  return requester(url, createOptions('DELETE'));
+}
+export default { get, post, put, del };
