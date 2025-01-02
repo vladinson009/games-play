0e2d4e054188f9c68fb421589307e53de233d85e
vladinSon <vladinson009@icloud.com>
2025-01-02 20:38:01 +0100

    added most recent games on home page

--- client/src/components/home/HomePage.jsx
+++ client/src/components/home/HomePage.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+import gameApi from "../../api/game";
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
+    const [games, setGames] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        (async function () {
+            const response = await gameApi.getAllGames()
+            const recentGames = Object.values(response).reverse().slice(0, 3);
+            setGames(recentGames)
+            setIsLoading(false);
+
+        })()
+
+    }, []);
+
 
     return (
         <section id="welcome-world">
@@ -11,46 +30,24 @@ export default function HomePage() {
             <div id="home-page">
                 <h1>Latest Games</h1>
 
-                {/* <!-- Display div: with information about every game (if any) --> */}
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/CoverFire.png" />
-                    </div>
-                    <h3>Cover Fire</h3>
-                    <div className="rating">
-                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Details</a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/ZombieLang.png" />
-                    </div>
-                    <h3>Zombie Lang</h3>
-                    <div className="rating">
-                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Details</a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/MineCraft.png" />
-                    </div>
-                    <h3>MineCraft</h3>
-                    <div className="rating">
-                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Details</a>
-                    </div>
-                </div>
-
-                {/* <!-- Display paragraph: If there is no games  --> */}
-                <p className="no-articles">No games yet</p>
+                {isLoading
+                    ? (<div>...Loading</div>)
+                    : (games.length > 0
+                        ? (games.map(game => (
+                            <div key={game._id} className="game">
+                                <div className="image-wrap">
+                                    <img src={game.imageUrl} />
+                                </div>
+                                <h3>{game.title}</h3>
+                                <div className="rating">
+                                    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                                </div>
+                                <div className="data-buttons">
+                                    <Link to={`/games/details/${game._id}`} className="btn details-btn">Details</Link>
+                                </div>
+                            </div>)))
+                        : (<p className="no-articles">No articles yet</p>)
+                    )}
             </div>
         </section>
     )
