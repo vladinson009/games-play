d073afe0561404891f5cf5adf4ed3690303bc3c9
vladinSon <vladinson009@icloud.com>
2025-01-02 20:35:56 +0100

    added authorized requests

--- client/src/api/fetchApi.js
+++ client/src/api/fetchApi.js
@@ -1,8 +1,14 @@
+import { deleteSession, getSession } from '../util/sesionStorage';
+
 const BASE_URL = 'http://localhost:3030';
 
 async function requester(url, options) {
   const response = await fetch(BASE_URL + url, options);
   if (!response.ok) {
+    if (response.status === 403) {
+      deleteSession();
+      throw new Error('Invalid credentials');
+    }
     throw new Error(`${response.status} - ${response.statusText}`);
   }
   if (response.status === 204) {
@@ -12,14 +18,17 @@ async function requester(url, options) {
 }
 
 function createOptions(method, body) {
+  const session = getSession();
   const options = {
     method,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: {},
   };
   if (body) {
     options.body = JSON.stringify(body);
+    options.headers['Content-Type'] = 'application/json';
+  }
+  if (session) {
+    options.headers['X-Authorization'] = session.token;
   }
   return options;
 }
