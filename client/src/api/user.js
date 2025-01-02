df1ade1099a7f2fc8fc0c3ab280ea0b5170b6727
vladinSon <vladinson009@icloud.com>
2025-01-02 20:36:54 +0100

    conditional statements for user api

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,13 +1,23 @@
 import fetchApi from './fetchApi';
 
 function login(email, password) {
+  if (!email.trim() || !password.trim()) {
+    throw new Error('All fields are required');
+  }
   return fetchApi.post('/users/login', { email, password });
 }
+
 function register(email, password, repass) {
   if (password !== repass) {
     throw new Error('Passwords does not match');
   }
+  if (!email.trim() || !password.trim() || !repass.trim()) {
+    throw new Error('All fields are required');
+  }
   return fetchApi.post('/users/register', { email, password });
 }
 
-export default { login, register };
+function logout() {
+  return fetchApi.get('/users/logout');
+}
+export default { login, register, logout };
