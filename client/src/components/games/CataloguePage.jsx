322697435e9b85061f76584521a4d1226ec73106
vladinSon <vladinson009@icloud.com>
2025-01-02 20:37:21 +0100

    functionality for catalogue page

--- client/src/components/games/CataloguePage.jsx
+++ client/src/components/games/CataloguePage.jsx
@@ -1,37 +1,36 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+import game from "../../api/game"
+
 export default function CataloguePage() {
+    const [games, setGames] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        setIsLoading(true);
+        (async function () {
+            const response = await game.getAllGames()
+            setGames(Object.values(response))
+            setIsLoading(false);
+        })()
+    }, [])
+
 
     return (
-        //  < !--Catalogue -- >
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* <!-- Display div: with information about every game (if any) --> */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
+            {isLoading ? <div>...Loading</div> : games.length > 0
+                ? games.map(game => (
+                    <div key={game._id} className="allGames">
+                        <div className="allGames-info">
+                            <img src={game.imageUrl} />
+                            <h6>{game.category}</h6>
+                            <h2>{game.title}</h2>
+                            <Link to={`/games/details/${game._id}`} className="details-button">Details</Link>
+                        </div>
+                    </div>))
+                : <h3 className="no-articles">No articles yet</h3>}
 
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            <h3 className="no-articles">No articles yet</h3>
         </section>
     )
 }
