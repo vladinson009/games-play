75cf6623524287aab52506bb9fd46f7e895792e1
vladinSon <vladinson009@icloud.com>
2025-01-02 20:38:47 +0100

    improved request for authorized logout

--- client/src/components/user/LogoutPage.jsx
+++ client/src/components/user/LogoutPage.jsx
@@ -1,13 +1,17 @@
 import { useContext, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import sessionContext from '../../util/sessionContext';
+import fetchApi from '../../api/fetchApi';
 
 export default function LogoutPage() {
     const context = useContext(sessionContext);
 
     useEffect(() => {
-        context.setSession({});
-        sessionStorage.removeItem('auth');
+        (async function logout() {
+            await fetchApi.get('/users/logout');
+            context.setSession({});
+            sessionStorage.removeItem('auth');
+        })()
     }, []);
 
     return <Navigate to="/" replace />;
