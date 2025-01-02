eeeaaf5d2586ad09b081313ec6185a540012c693
vladinSon <vladinson009@icloud.com>
2025-01-02 20:37:48 +0100

    functionality for details page

--- client/src/components/games/DetailsPage.jsx
+++ client/src/components/games/DetailsPage.jsx
@@ -1,4 +1,21 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect, useContext } from "react"
+
+import gameApi from "../../api/game"
+import sessionContext from "../../util/sessionContext"
 export default function DetailsPage() {
+    const { gameId } = useParams()
+    const [game, setGame] = useState({})
+    const { session } = useContext(sessionContext)
+    const isCreator = session.userId === game._ownerId
+
+    useEffect(() => {
+        (async function () {
+            const response = await gameApi.getGameById(gameId);
+            setGame(response)
+        })()
+    }, [])
+
 
     return (
         //   < !--Details Page-- >
@@ -6,18 +23,13 @@ export default function DetailsPage() {
             <h1>Game Details</h1>
             <div className="info-section">
                 <div className="game-header">
-                    <img className="game-img" src="images/MineCraft.png" />
+                    <img className="game-img" src={game.imageUrl} />
                     <h1>Bright</h1>
-                    <span className="levels">MaxLevel: 4</span>
-                    <p className="type">Action, Crime, Fantasy</p>
+                    <span className="levels">MaxLevel: {game.maxLevel}</span>
+                    <p className="type">{game.category}</p>
                 </div>
 
-                <p className="text">
-                    Set in a world where fantasy creatures live side by side with humans. A human cop is
-                    forced to work with an Orc to find a weapon everyone is prepared to kill for. Set in a
-                    world where fantasy creatures live side by side with humans. A human cop is forced to
-                    work with an Orc to find a weapon everyone is prepared to kill for.
-                </p>
+                <p className="text">{game.summary}</p>
 
                 {/* // <!-- Bonus ( for Guests and Users ) --> */}
                 <div className="details-comments">
