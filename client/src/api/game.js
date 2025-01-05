0b8eb5ca673847e277885fc3a9d367f52fe2193a
vladinSon <vladinson009@icloud.com>
2025-01-05 22:48:51 +0100

    migrated db to collections & added paginations and sorting

--- client/src/api/game.js
+++ client/src/api/game.js
@@ -1,13 +1,16 @@
 import fetchApi from './fetchApi';
 
 function getAllGames() {
-  return fetchApi.get('/jsonstore/games');
+  return fetchApi.get('/data/games?sortBy=title');
+}
+function getRecentGames() {
+  return fetchApi.get(`/data/games?sortBy=_createdOn%20desc&pageSize=3`);
 }
 function getGameById(gameId) {
-  return fetchApi.get(`/jsonstore/games/${gameId}`);
+  return fetchApi.get(`/data/games/${gameId}`);
 }
 function deleteGameById(gameId) {
-  return fetchApi.del(`/jsonstore/games/${gameId}`);
+  return fetchApi.del(`/data/games/${gameId}`);
 }
 function createGame(game) {
   Object.values(game).forEach((el) => {
@@ -15,7 +18,7 @@ function createGame(game) {
       throw new Error('All fields are required');
     }
   });
-  return fetchApi.post('/jsonstore/games', game);
+  return fetchApi.post('/data/games', game);
 }
 function updateGame(game, gameId) {
   Object.values(game).forEach((el) => {
@@ -23,16 +26,22 @@ function updateGame(game, gameId) {
       throw new Error('All fields are required');
     }
   });
-  return fetchApi.put('/jsonstore/games/' + gameId, game);
+  return fetchApi.put('/data/games/' + gameId, game);
 }
-function addComment(gameId, comment) {
-  return fetchApi.post(`/jsonstore/comments/${gameId}`, comment);
+function addComment(comment) {
+  for (const el in comment) {
+    if (comment[el] == '') {
+      throw new Error('Can not send empty comment! :)');
+    }
+  }
+  return fetchApi.post(`/data/comments/`, comment);
 }
 function getComments(gameId) {
-  return fetchApi.get(`/jsonstore/comments/${gameId}`);
+  return fetchApi.get(`/data/comments?where=_gameId%3D%22${gameId}%22`);
 }
 export default {
   getAllGames,
+  getRecentGames,
   getGameById,
   createGame,
   addComment,
