ee8f0761faf6f666c2422b6f8dc88214fa976214
vladinSon <vladinson009@icloud.com>
2025-01-07 23:05:31 +0100

    use custom hook for login page

--- client/src/components/user/LoginPage.jsx
+++ client/src/components/user/LoginPage.jsx
@@ -1,29 +1,9 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext, useState } from 'react';
-
-import sessionContext from '../../util/sessionContext';
+import { Link } from 'react-router-dom';
+import useLogin from '../../hooks/user/useLogin';
 import ErrorModal from '../ErrorModal';
-import userApi from '../../api/user';
-import { getSession, setSession } from '../../util/sesionStorage';
 
 export default function LoginPage() {
-    const [error, setError] = useState(null);
-    const [userData, setUserData] = useState({ email: '', password: '' });
-
-    const navigate = useNavigate();
-    const sessionCtx = useContext(sessionContext);
-
-    async function onSubmit(e) {
-        e.preventDefault();
-        try {
-            const user = await userApi.login(userData.email, userData.password);
-            setSession(user);
-            sessionCtx.setSession(getSession());
-            navigate('/');
-        } catch (err) {
-            setError(err.message);
-        }
-    }
+    const { error, setError, userData, setUserData, onSubmit } = useLogin()
 
     return (
         <section id="login-page" className="auth">
