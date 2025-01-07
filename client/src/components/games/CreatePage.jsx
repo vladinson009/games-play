9514cae370f67e05c1f6c98fbb4cb07620c3fa14
vladinSon <vladinson009@icloud.com>
2025-01-07 23:04:35 +0100

    use custom hook for create page

--- client/src/components/games/CreatePage.jsx
+++ client/src/components/games/CreatePage.jsx
@@ -1,42 +1,13 @@
-import { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
 
-import sessionContext from "../../util/sessionContext";
-import gameApi from "../../api/game";
-
+import useCreateGame from '../../hooks/game/useCreateGame'
 import ErrorModal from "../ErrorModal";
 
 export default function CreatePage() {
-    const [error, setError] = useState(null);
-    const [userInput, setUserInput] = useState({
-        title: "",
-        category: "",
-        maxLevel: "",
-        imageUrl: "",
-        summary: "",
-        _ownerId: ""
-    })
-    const { session } = useContext(sessionContext);
-    const navigate = useNavigate()
-    async function onSubmit(e) {
-        e.preventDefault();
-        const { title, category, maxLevel, imageUrl, summary } = userInput;
-        const game = {
-            title,
-            category,
-            maxLevel,
-            imageUrl,
-            summary,
-            _ownerId: session._id,
-            _createdOn: Date.now(),
-        }
-        try {
-            const response = await gameApi.createGame(game);
-            navigate(`/games/details/${response._id}`);
-        } catch (error) {
-            setError(error.message);
-        }
-    }
+    const { setError,
+        error,
+        setUserInput,
+        userInput,
+        onSubmit } = useCreateGame()
     return (
 
         <section id="create-page" className="auth">
