a6d75708251aeef4250d266d9f1baf20712bb7ea
vladinSon <vladinson009@icloud.com>
2025-01-07 23:04:43 +0100

    use custom hook for details page

--- client/src/components/games/DetailsPage.jsx
+++ client/src/components/games/DetailsPage.jsx
@@ -1,51 +1,19 @@
-import { useParams, Link } from "react-router-dom"
-import { useState, useEffect, useContext } from "react"
-
-import gameApi from "../../api/game"
-import sessionContext from "../../util/sessionContext"
+import { Link } from 'react-router-dom';
 import ErrorModal from "../ErrorModal"
+import useDetailsPage from '../../hooks/game/useDetailsPage';
+
+useDetailsPage
+
 export default function DetailsPage() {
-    const { gameId } = useParams()
-    const { session } = useContext(sessionContext)
-
-    const [game, setGame] = useState({})
-    const [comment, setComment] = useState("")
-    const [error, setError] = useState(null)
-
-
-    const isCreator = session._id === game._ownerId
-
-    useEffect(() => {
-        (async function () {
-            const [response, comments] = await Promise.all([
-                gameApi.getGameById(gameId),
-                gameApi.getComments(gameId)
-            ])
-            response.comments = Object.values(comments);
-            setGame(response)
-
-
-        })()
-    }, [])
-
-    async function onComment(e) {
-        e.preventDefault();
-        const newComment = {
-            _gameId: gameId,
-            _ownerId: session._id,
-            owner: session.email,
-            text: comment
-        }
-        try {
-            const response = await gameApi.addComment(newComment)
-            setGame({ ...game, comments: [...game.comments, response] })
-            setComment("")
-
-
-        } catch (error) {
-            setError(error.message)
-        }
-    }
+    const { error,
+        setError,
+        comment,
+        setComment,
+        onComment,
+        isCreator,
+        session,
+        gameId,
+        game } = useDetailsPage()
 
     return (
         //   < !--Details Page-- >
