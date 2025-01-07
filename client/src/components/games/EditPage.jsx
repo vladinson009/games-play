dbad2f6a1db9287fbf0c2dee73d177eecae21ba3
vladinSon <vladinson009@icloud.com>
2025-01-07 23:05:16 +0100

    use custom hook for edit

--- client/src/components/games/EditPage.jsx
+++ client/src/components/games/EditPage.jsx
@@ -1,54 +1,13 @@
-import { useNavigate, useParams } from "react-router-dom"
-import { useState, useEffect } from "react";
-
+import useEditPage from "../../hooks/game/useEditPage";
 import ErrorModal from "../ErrorModal"
-import gameApi from "../../api/game"
 
 export default function EditPage() {
-    const { gameId } = useParams();
-    const navigate = useNavigate();
-    const defaultState = {
-        title: "",
-        category: "",
-        maxLevel: "",
-        imageUrl: "",
-        summary: "",
-        _ownerId: ""
-    }
-    const [userInput, setUserInput] = useState(defaultState);
-    const [error, setError] = useState(null);
 
-    useEffect(() => {
-        (async function () {
-            try {
-                const response = await gameApi.getGameById(gameId)
-                setUserInput(response);
-            } catch (error) {
-                setError(error.message);
-            }
-        })()
-    }, []);
-
-    async function onSubmit(e) {
-        e.preventDefault();
-        const { title, category, maxLevel, imageUrl, summary, _ownerId, _id } = userInput;
-        const game = {
-            title,
-            category,
-            maxLevel,
-            imageUrl,
-            summary,
-            _ownerId,
-            _id
-        }
-        try {
-            await gameApi.updateGame(game, gameId);
-            navigate(`/games/details/${userInput._id}`);
-        } catch (error) {
-            setError(error.message);
-        }
-    }
-
+    const { error,
+        setError,
+        userInput,
+        setUserInput,
+        onSubmit, } = useEditPage()
 
     return (
         //    < !--Edit Page(Only for the creator )-->
